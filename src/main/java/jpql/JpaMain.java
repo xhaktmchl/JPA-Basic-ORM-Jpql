edbf8ac4b05d6c40be9cff3624a0a3ed74f25a1f
xhaktmchl <xhaktmchl@naver.com>
2022-07-27 19:30:51 +0900

    feat: JPQL기본, 파라미터 바인딩, 프로젝션, 페이징 구현

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
@@ -18,6 +15,82 @@ public class JpaMain {
         tx.begin();
 
         try{
+            System.out.println("====1");
+            Member m1 = new Member();
+            m1.setUsername("member1");
+            m1.setAge(20);
+            em.persist(m1);
+            System.out.println("====1");
+            em.flush();
+            em.clear();
+            System.out.println("====1");
+            /*JPQL*/
+            /*
+             TypeQuery, Query;
+            * */
+            System.out.println("====쿼리형");
+            TypedQuery<Member> query1 = em.createQuery("select m from Member as m", Member.class); // 반환타입 명확할 때 사용
+            Query query2 = em.createQuery("select m.username, m.age from Member as m");  // 반환 타입 잘 모를 때 사용
+
+            // 조회
+            query1.getResultList(); // 히스트 조회
+            //query1.getSingleResult(); // 갯수가 1개가 아니면 에러나기 때문에 지양
+
+
+            // 파라미터 바인딩
+            System.out.println("====바인딩1");
+            Member result1 =  em.createQuery("select m from Member as m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println(result1.getUsername());
+            System.out.println("====바인딩2");
+            List<Member> result2 =  em.createQuery("select m from Member as m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getResultList();
+            Member findM1 = result2.get(0);
+            findM1.setAge(21);
+
+            /* JPQL 프로젝션*/
+            System.out.println("====프로젝션");
+            // 조인 : sql 처럼 조인을 명시하는 것을 추천
+            List<Team> result3= em.createQuery("select t from Member as m join m.team as t", Team.class)
+                    .getResultList();
+
+            // 스칼라 타입 프로젝션, 중복제거
+            System.out.println("====스칼라 프로젝션");
+            em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+
+
+            /*여러 값 프로젝션*/
+            System.out.println("====여러값 프로젝션");
+            //2.Object[]타입으로 조회
+            System.out.println("====여러값 프로젝션2");
+            List<Object[]> result4 = em.createQuery("select m.username, m.age from Member as m")
+                    .getResultList();
+            Object[] result5 = result4.get(0);
+            System.out.println("username=" + result5[0]);
+            System.out.println("age=" + result5[1]);
+
+            //3.new 명령어로 조회
+            System.out.println("====여러값 프로젝션3");
+            List<MemberDTO> result6 = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member as m", MemberDTO.class)
+                    .getResultList();
+            MemberDTO findMDto1 = result6.get(0);
+            System.out.println("username=" + findMDto1.getName());
+            System.out.println("age=" + findMDto1.getAge());
+
+
+            /*
+            페이징
+             */
+            List<Member> result7 = em.createQuery("select m from Member as m order by m.age desc",Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+            for(Member m: result7){
+                System.out.println("member = " + m.getUsername());
+            }
 
 
             // 쓰기지연 sql 저장소에 모든 sql 실행
--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -9,10 +9,45 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     private String username;
     private int age;
+
+
+    /*연관간계 편의 메서드*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
